95aead777b0996ecac3099e2e85308b05029e6c0
Dale Higgs <dale3h@gmail.com>
2016-08-15 17:14:18 -0500

    fix telegram component error

--- custom_components/telegram.py
+++ custom_components/telegram.py
@@ -9,49 +9,82 @@ import logging
 import voluptuous as vol
 
 from homeassistant.const import CONF_API_KEY
-import homeassistant.helpers.config_validation as cv
 
 REQUIREMENTS = ['telepot==8.3']
 DOMAIN = 'telegram'
 
-# CONF_CHAT_IDS = 'chat_ids'
-# CONF_USERS = 'users'
-# CONF_ADMINS = 'admins'
+CONF_CHAT_IDS = 'chat_ids'
+CONF_USERS = 'users'
+CONF_ADMINS = 'admins'
 
-PLATFORM_SCHEMA = vol.Schema({
-    vol.Required(CONF_API_KEY): cv.string,
-#     vol.Required(CONF_CHAT_IDS): cv.string,
-#     vol.Required(CONF_USERS): cv.string,
-#     vol.Optional(CONF_ADMINS): cv.string,
-})
-
-API_KEY = None
-# CHAT_IDS = None
-# USERS = None
-# ADMINS = None
+EVENT_INCOMING_BOT_MESSAGE = 'incoming_bot_message'
 
 TELEGRAM = None
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_API_KEY): str,
+#         vol.Required(CONF_CHAT_IDS): str,
+#         vol.Required(CONF_USERS): str,
+#         vol.Optional(CONF_ADMINS): str
+    })
+}, extra=vol.ALLOW_EXTRA)
 
 _LOGGER = logging.getLogger(__name__)
 
 
+class TelegramBot(object):
+    """Telegram Bot object"""
+
+    def __init__(self, hass, config):
+        """Initialize the Telegram Bot object."""
+        import telepot
+
+        conf = config[DOMAIN]
+
+        api_key = conf.get(CONF_API_KEY)
+        chat_ids = conf.get(CONF_CHAT_IDS)
+        users = conf.get(CONF_USERS)
+        admins = conf.get(CONF_ADMINS)
+
+        _LOGGER.warning('Telegram Chat IDs: %s', chat_ids)
+        _LOGGER.warning('Telegram Users: %s', users)
+        _LOGGER.warning('Telegram Admins: %s', admins)
+
+        self._hass = hass
+
+        self._chat_ids = chat_ids
+        self._users = users
+        self._admins = admins
+
+        self._bot = telepot.Bot(api_key)
+        self._bot.message_loop(self.handle)
+
+        self._me = self._bot.getMe()
+
+        _LOGGER.info("Telegram bot is '%s'.", self._me.username)
+
+    def handle(self, message):
+        message = self.parse(message)
+
+        _LOGGER.warning('Incoming Telegram: %s', message)
+
+        self._hass.bus.fire(EVENT_INCOMING_BOT_MESSAGE, message)
+
+    def parse(self, message):
+        import telepot
+
+        flavor = telepot.flavor(message)
+        summary = telepot.glance(message, flavor=flavor)
+
+        message['flavor'] = flavor
+        message['summary'] = summary
+
+        return message
+
+
 def setup(hass, config):
-    global TELEGRAM, API_KEY
-#     global API_KEY, CHAT_IDS, USERS, ADMINS
-
-    conf = config[DOMAIN]
-
-    API_KEY = conf[CONF_API_KEY]
-#     CHAT_IDS = conf[CONF_CHAT_IDS]
-#     USERS = conf[CONF_USERS]
-#     ADMINS = conf[CONF_ADMINS]
-
-    import telepot
-
-    TELEGRAM = telepot.Bot(API_KEY)
-    TELEGRAM.message_loop(handle_message)
+    global TELEGRAM
+    TELEGRAM = TelegramBot(hass, config)
 
     return True
-
-def handle_message(message):
-    _LOGGER.info('Incoming Telegram:', message)
