8438295d3c93dfeacd3d036cde11e4910fa6cb90
Dale Higgs <dale3h@gmail.com>
2016-09-01 11:21:34 -0500

    Add shell scripts

--- shell_scripts/backup_config.sh
+++ shell_scripts/backup_config.sh
@@ -1,1 +1,9 @@
-../../shell/backup_config.sh
+#!/bin/bash
+
+BACKUP_FILE=/home/pi/hass-config_$(date +"%Y%m%d_%H%M%S").zip
+
+pushd /home/pi/.homeassistant >/dev/null
+zip -9 -q -r $BACKUP_FILE . -x"components/*" -x"deps/*" -x"home-assistant.db" -x"home-assistant_v2.db" -x"home-assistant.log"
+popd >/dev/null
+
+echo Backup complete: $BACKUP_FILE
--- shell_scripts/update_hass.sh
+++ shell_scripts/update_hass.sh
@@ -1,1 +1,10 @@
-../../shell/update_hass.sh
+#!/bin/bash
+
+# Run backup first
+/home/pi/shell/backup_config.sh
+
+# Run update
+pushd /home/pi >/dev/null
+sudo pip3 install --upgrade homeassistant
+hassctl restart
+popd >/dev/null
