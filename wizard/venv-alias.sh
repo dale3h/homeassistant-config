9a44e0709385e148bcbf8d05ed53d8be3437bc9a
Dale Higgs <dale3h@gmail.com>
2017-02-18 13:00:25 -0600

    Fix venv-alias script to set shell

--- wizard/venv-alias.sh
+++ wizard/venv-alias.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # Install by running:
-# curl -sL https://raw.githubusercontent.com/dale3h/homeassistant-config/master/wizard/venv-alias.sh | bash -
+#   curl -sL https://raw.githubusercontent.com/dale3h/homeassistant-config/master/wizard/venv-alias.sh | bash -
+#
+# You can also set the Home Assistant username and virtualenv path using:
+#   curl -sL https://raw.githubusercontent.com/dale3h/homeassistant-config/master/wizard/venv-alias.sh | \
+#     HASS_USER=hass \
+#     VIRTUAL_ENV=/srv/hass/hass_venv \
+#     bash -
 
-HASS_USER=homeassistant
-VIRTUAL_ENV=/srv/homeassistant
+
+HASS_USER=${HASS_USER:-homeassistant}
+VIRTUAL_ENV=${VIRTUAL_ENV:-/srv/homeassistant}
 
 ALIAS_FILE=$HOME/.bash_aliases
 PROFILE_FILE=/home/$HASS_USER/.profile
 
 add_venv_alias() {
-  printf "\nalias venv='sudo su - $HASS_USER'\n" >> $ALIAS_FILE
+  printf "\nalias venv='sudo su -s /bin/bash - $HASS_USER'\n" >> $ALIAS_FILE
 
   sudo -u $HASS_USER -i /bin/bash <<EOF
   printf "\nsource $VIRTUAL_ENV/bin/activate\n" >> $PROFILE_FILE
