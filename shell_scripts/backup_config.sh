417bac137ef1c54cbe3586e9d567674b72679962
Dale Higgs <dale3h@gmail.com>
2017-02-18 12:20:43 -0600

    Update shell scripts

--- shell_scripts/backup_config.sh
+++ shell_scripts/backup_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG_DIR="/etc/homeassistant"
-BACKUP_FILE="/srv/homeassistant/backup/homeassistant_$1.zip"
+CONFIG_DIR=/etc/homeassistant
+BACKUP_FILE=/srv/homeassistant/backup/homeassistant_$(date +"%Y%m%d_%H%M").zip
 
 pushd $CONFIG_DIR >/dev/null
 zip -9 -q -r $BACKUP_FILE . -x"components/*" -x"deps/*" -x"home-assistant.db" -x"home-assistant_v2.db" -x"home-assistant.log"
--- shell_scripts/update_hass.sh
+++ shell_scripts/update_hass.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+VIRTUAL_ENV=/srv/homeassistant
+HASS_SERVICE=home-assistant.service
+
 # Create a backup
-/etc/homeassistant/shell_scripts/backup_config.sh pre_update
+/etc/homeassistant/shell_scripts/backup_config.sh
 
 # Activate virtualenv
-source /srv/homeassistant/bin/activate
+source $VIRTUAL_ENV/bin/activate
 
 # Upgrade Home Assistant
 pip3 install --upgrade homeassistant
@@ -15,4 +18,5 @@ deactivate
 # Restart Home Assistant
 # This requires a modification using `sudo visudo`:
 #   homeassistant ALL=(ALL) NOPASSWD: /bin/systemctl restart home-assistant.service
-sudo systemctl restart home-assistant.service
+# Make sure to change `home-assistant.service` to the correct service name
+sudo systemctl restart $HASS_SERVICE
